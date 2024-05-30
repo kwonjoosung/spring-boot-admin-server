dfef57626a996b4ad65e1cebc17b0d823e7508a4
권주성 <joosung.kwon@medit.com>
2024-05-30 14:02:42 +0900

    feat: AdminServer 활성화

--- src/main/java/com/meditlink/springbootadminserver/SpringBootAdminServerApplication.java
+++ src/main/java/com/meditlink/springbootadminserver/SpringBootAdminServerApplication.java
@@ -4,6 +4,7 @@ import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableAdminServer
 @SpringBootApplication
 public class SpringBootAdminServerApplication {
 
