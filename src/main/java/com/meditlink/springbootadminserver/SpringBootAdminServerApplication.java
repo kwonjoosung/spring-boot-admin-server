e285e396a93d4750dbdbcf9b6f987ccff7830fa5
권주성 <joosung.kwon@medit.com>
2024-05-30 14:01:24 +0900

    init project

---
+++ src/main/java/com/meditlink/springbootadminserver/SpringBootAdminServerApplication.java
@@ -0,0 +1,14 @@
+package com.meditlink.springbootadminserver;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAdminServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootAdminServerApplication.class, args);
+    }
+
+}
